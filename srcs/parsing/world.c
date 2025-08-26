c9d7d1dcf75b1596f45216cffd2a12f82af58634
Thiwanka Somachandra <thivankas@outlook.com>
2025-08-26 11:03:40 +0300

    fix world data parsing isuues

--- srcs/parsing/world.c
+++ srcs/parsing/world.c
@@ -39,19 +39,11 @@ void	free_split(char **array)
 int	init_world(t_world *world, float *v)
 {
 	if (world->set_ambient)
-		return (1);
-	if (v[0] < 0.0f || v[0] > 1.0f)
-		return (1);
-	if (v[1] < 0.0f || v[1] > 255.0f)
-		return (1);
-	if (v[2] < 0.0f || v[2] > 255.0f)
-		return (1);
-	if (v[3] < 0.0f || v[3] > 255.0f)
-		return (1);
+		return (0);
 	world->ambient = v[0];
 	color(&world->colour, v[1] / 255.0f, v[2] / 255.0f, v[3] / 255.0f);
 	world->set_ambient = true;
-	return (0);
+	return (1);
 }
 
 /**
@@ -70,35 +62,16 @@ bool	valid_char(char c)
 	return (false);
 }
 
-/**
- * Validates a comma-separated triple "x,y,z" with only numeric chars,
- * '-' and '.'.
- * Requires exactly two commas and allows end-of-string or whitespace after
- * the triple.
- */
-int	values_validation(char *str)
+bool	get_ambient_brigthness(float *v, const char *line)
 {
-	int	i;
-	int	count;
+	float	d;
 
-	i = 0;
-	count = 0;
-	while (str[i])
-	{
-		if (str[i] == ',')
-			count++;
-		else if (valid_char(str[i]))
-		{
-			i++;
-			continue ;
-		}
-		else
-			break ;
-		i++;
-	}
-	if (count == 2 && (str[i] == '\0' || ft_isspace(str[i])))
-		return (1);
-	return (0);
+	if (!ft_atof(line, &d))
+		return (false);
+	if (d < 0.0f || d > 1.0f)
+		return (false);
+	v[0] = d;
+	return (true);
 }
 
 /**
@@ -109,28 +82,19 @@ int	values_validation(char *str)
 int	set_world(char *line, t_state *state, int *index)
 {
 	char	**items;
-	char	**values;
 	float	v[4];
 
+	if (!line)
+		return (0);
 	items = ft_split(&line[*index], ' ');
 	if (!items || !items[0] || !items[1])
-		return (free_split(items), 1);
-	if (!items[1] || !values_validation(items[1]))
-		return (free_split(items), 1);
-	values = ft_split(items[1], ',');
-	if (!values || !values[0] || !values[1] || !values[2])
-	{
-		free_split(items);
-		free_split(values);
-		return (1);
-	}
-	v[0] = ft_atof(items[0]);
-	v[1] = ft_atof(values[0]);
-	v[2] = ft_atof(values[1]);
-	v[3] = ft_atof(values[2]);
+		return (free_split(items), 0);
+	if (!get_ambient_brigthness(v, items[0]))
+		return (free_split(items), 0);
+	if (!extract_color(items[1], &v[1], &v[2], &v[3]))
+		return (free_split(items), 0);
 	free_split(items);
-	free_split(values);
-	if (init_world(&state->world, v))
-		return (1);
-	return (0);
+	if (!init_world(&state->world, v))
+		return (0);
+	return (1);
 }
